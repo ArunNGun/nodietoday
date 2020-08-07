d3c3fc8f2b2fa50fda7291681ab136248f8ff995
C4PT4INNULL <chaudharyarun5797@gmail.com>
2020-08-07 05:53:47 +0530

    Updates

---
+++ precache-manifest.59958c00da637b3540612335a940b9e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97b6f9ecc216b4ad3e89b85934f0779",
+    "url": "/nodietoday/index.html"
+  },
+  {
+    "revision": "68e5144e40c731810efc",
+    "url": "/nodietoday/static/css/main.a2a48b9d.chunk.css"
+  },
+  {
+    "revision": "eba335c0e72f303cf627",
+    "url": "/nodietoday/static/js/2.e6ed9948.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nodietoday/static/js/2.e6ed9948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e5144e40c731810efc",
+    "url": "/nodietoday/static/js/main.130876c8.chunk.js"
+  },
+  {
+    "revision": "d863bdcac407623f20b2",
+    "url": "/nodietoday/static/js/runtime-main.f4ae963e.js"
+  },
+  {
+    "revision": "3ae8ff155c7d622ad1472c627b11f8fc",
+    "url": "/nodietoday/static/media/audio.3ae8ff15.mp3"
+  },
+  {
+    "revision": "0a1cf2afec1ca331d4127e8bac4e6f07",
+    "url": "/nodietoday/static/media/image1.0a1cf2af.gif"
+  },
+  {
+    "revision": "446a5883b8914f3fe7376836a3c7e1f6",
+    "url": "/nodietoday/static/media/vinyl.446a5883.png"
+  }
+]);
